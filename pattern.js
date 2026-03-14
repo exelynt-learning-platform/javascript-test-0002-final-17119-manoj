fbec8f5e675fb39c7c6f3d25273e0cfb25411282
manojkhandve <manojkhandve707@gmail.com>
2026-03-14 17:33:28 +0530

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -1,18 +1,18 @@
 let n = 5;
 
 for (let i = n; i >= 1; i--) {
-    
-    // print spaces
+    let row = "";
+
     for (let s = 0; s < n - i; s++) {
-        process.stdout.write(" ");
+        row += " ";
     }
 
     let num = 1;
 
     for (let j = 1; j <= i; j++) {
-        process.stdout.write(num + " ");
+        row += num + " ";
         num = num * (i - j) / j;
     }
 
-    console.log();
+    console.log(row);
 }
